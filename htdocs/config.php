50d018a02f913a98391ac2ff678559483b2b5bda
Christian Elmer <christian.elmer@sap.com>
2018-05-24 23:50:59 +0200

    add config.php

---
+++ htdocs/config.php
@@ -0,0 +1,7 @@
+<?php
+
+$conf               = array();
+$conf['base_url']   = ""; // usually => "", generally: end with trailing slash
+$conf['base_path']  = "/home/pi/src/RPi-Jukebox-RFID"; // absolute path to folder
+
+?>
