c2cb5ef0216c6a5922fd227935bb18760c089b3c
Christian Elmer <christian@keinkurt.de>
2018-05-24 21:34:01 +0000

    play sound after successfully read card

--- scripts/daemon_rfid_reader.py
+++ scripts/daemon_rfid_reader.py
@@ -9,11 +9,27 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 print dir_path
 
-while True:
-        # reading the card id
-        cardid = reader.readCard()
+# Capture SIGINT for cleanup when the script is aborted
+def end_read(signal, frame):
+    global continue_reading
+    print "Ctrl+C captured, ending read."
+    continue_reading = False
+    GPIO.cleanup()
+
+
+# Welcome message
+print "Press Ctrl-C to stop."
+
+# Hook the SIGINT
+signal.signal(signal.SIGINT, end_read)
+
+while continue_reading:
+    # reading the card id
+    cardid = reader.read_card()
+    if cardid is not None:
+        subprocess.call(['aplay ' + dir_path + '/../misc/robot_blip.wav'], shell=True)
         try:
-            # start the player script and pass on the cardid 
+            # start the player script and pass on the card id
             subprocess.call([dir_path + '/rfid_trigger_play.sh --cardid=' + cardid], shell=True)
         except OSError as e:
-            print "Execution failed:" 
+            print "Execution failed:" + str(e)
---
+++ scripts/rfid_trigger_play.sh
@@ -0,0 +1,263 @@
+#!/bin/bash
+
+# Reads the card ID or the folder name with audio files
+# from the command line (see Usage).
+# Then attempts to get the folder name from the card ID
+# or play audio folder content directly
+#
+# Usage for card ID
+# ./rfid_trigger_play.sh -c=1234567890
+# or
+# ./rfid_trigger_play.sh --cardid=1234567890
+#
+# For folder names:
+# ./rfid_trigger_play.sh -d=foldername
+# or
+# ./rfid_trigger_play.sh --dir=foldername
+
+# VARIABLES TO CHANGE
+# adjust these variables to match your system and configuration
+
+# If you use cards to change audio level, stop playing or shutdown the RPi,
+# replace the following strings with the ID of the card. For example:
+# Using the card ID 1234567890 to set the audio to mute, change this line:
+# CMDMUTE="mute"
+# to the following:
+# CMDMUTE="1234567890"
+# Leave everything untouched where you do not use a card.
+CMDMUTE="%CMDMUTE%"
+CMDVOL30="%CMDVOL30%"
+CMDVOL50="%CMDVOL50%"
+CMDVOL75="%CMDVOL75%"
+CMDVOL80="%CMDVOL80%"
+CMDVOL85="%CMDVOL85%"
+CMDVOL90="%CMDVOL90%"
+CMDVOL95="%CMDVOL95%"
+CMDVOL100="%CMDVOL100%"
+CMDVOLUP="plus"
+CMDVOLDOWN="minus"
+CMDSTOP="%CMDSTOP%"
+CMDSHUTDOWN="menu"
+CMDREBOOT="%CMDREBOOT%"
+# The following commands control VLC playout
+# next and prev play the preivous or next track in the playlist (== folder)
+CMDNEXT="forward"
+CMDPREV="rewind"
+# pause VLC playout
+CMDPAUSE="play"
+# resume VLC playout (makes only sense in combination with pause)
+CMDPLAY="play"
+
+# The absolute path to the folder whjch contains all the scripts.
+# Unless you are working with symlinks, leave the following line untouched.
+PATHDATA="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+##############################
+# NO CHANGES BENEATH THIS LINE
+
+# Get args from command line (see Usage above)
+for i in "$@"
+do
+case $i in
+    -c=*|--cardid=*)
+    CARDID="${i#*=}"
+    ;;
+    -d=*|--dir=*)
+    FOLDERNAME="${i#*=}"
+    ;;
+esac
+done
+
+# Set the date and time of now
+NOW=`date +%Y-%m-%d.%H:%M:%S`
+
+##################################################################
+# Check if we got the card ID or the audio folder from the prompt.
+# Sloppy error check, because we assume the best.
+if [ "$CARDID" ]; then
+    # we got the card ID
+    # If you want to see the CARDID printed, uncomment the following line
+    # echo CARDID = $CARDID
+
+    # If the input is of 'special' use, don't treat it like a trigger to play audio.
+    # Special uses are for example volume changes, skipping, muting sound.
+
+    if [ "$CARDID" == "$CMDMUTE" ]
+    then
+        # amixer sset 'PCM' 0%
+        $PATHDATA/playout_controls.sh -c=mute
+        
+    elif [ "$CARDID" == "$CMDVOL30" ]
+    then
+        # amixer sset 'PCM' 30%
+        $PATHDATA/playout_controls.sh -c=setvolume -v=30
+    
+    elif [ "$CARDID" == "$CMDVOL50" ]
+    then
+        # amixer sset 'PCM' 50%
+        $PATHDATA/playout_controls.sh -c=setvolume -v=50
+    
+    elif [ "$CARDID" == "$CMDVOL75" ]
+    then
+        # amixer sset 'PCM' 75%
+        $PATHDATA/playout_controls.sh -c=setvolume -v=75
+    
+    elif [ "$CARDID" == "$CMDVOL85" ]
+    then
+        # amixer sset 'PCM' 85%
+        $PATHDATA/playout_controls.sh -c=setvolume -v=85
+    
+    elif [ "$CARDID" == "$CMDVOL90" ]
+    then
+        # amixer sset 'PCM' 90%
+        $PATHDATA/playout_controls.sh -c=setvolume -v=90
+    
+    elif [ "$CARDID" == "$CMDVOL95" ]
+    then
+        # amixer sset 'PCM' 95%
+        $PATHDATA/playout_controls.sh -c=setvolume -v=95
+    
+    elif [ "$CARDID" == "$CMDVOL100" ]
+    then
+        # amixer sset 'PCM' 100%
+        $PATHDATA/playout_controls.sh -c=setvolume -v=100
+    
+    elif [ "$CARDID" == "$CMDVOLUP" ]
+    then
+        # amixer sset 'PCM' 500+
+        $PATHDATA/playout_controls.sh -c=volumeup   
+    
+    elif [ "$CARDID" == "$CMDVOLDOWN" ]
+    then
+        # amixer sset 'PCM' 500-
+        $PATHDATA/playout_controls.sh -c=volumedown
+    
+    elif [ "$CARDID" == "$CMDSTOP" ]
+    then
+        # kill all running VLC media players
+        # sudo pkill vlc
+        $PATHDATA/playout_controls.sh -c=playerstop
+    
+    elif [ "$CARDID" == "$CMDSHUTDOWN" ]
+    then
+        # shutdown the RPi nicely
+        # sudo halt
+        $PATHDATA/playout_controls.sh -c=shutdown
+        
+    elif [ "$CARDID" == "$CMDREBOOT" ]
+    then
+        # shutdown the RPi nicely
+        # sudo reboot
+        $PATHDATA/playout_controls.sh -c=reboot
+        
+    elif [ "$CARDID" == "$CMDNEXT" ]
+    then
+        # play next track in playlist (==folder)
+        # echo "next" | nc.openbsd -w 1 localhost 4212
+        $PATHDATA/playout_controls.sh -c=playernext
+        
+    elif [ "$CARDID" == "$CMDPREV" ]
+    then
+        # play previous track in playlist (==folder)
+        # echo "prev" | nc.openbsd -w 1 localhost 4212
+        $PATHDATA/playout_controls.sh -c=playerprev
+        
+    elif [ "$CARDID" == "$CMDPAUSE" ]
+    then
+        # pause current track
+        # echo "pause" | nc.openbsd -w 1 localhost 4212
+        $PATHDATA/playout_controls.sh -c=playerpause
+        
+    elif [ "$CARDID" == "$CMDPLAY" ]
+    then
+        # play / resume current track
+        # echo "play" | nc.openbsd -w 1 localhost 4212
+        $PATHDATA/playout_controls.sh -c=playerplay
+        
+    else
+        # We checked if the card was a special command, seems it wasn't.
+        # Now we expect it to be a trigger for one or more audio file(s).
+        # Let's look at the ID, write a bit of log information and then try to play audio.
+    
+        # Expected folder structure:
+        #
+        # $PATHDATA + /../shared/audiofolders/ + $FOLDERNAME
+        # Note: $FOLDERNAME is read from a file inside 'shortcuts'.
+        #       See manual for details
+        #
+        # Example:
+        #
+        # $PATHDATA/../shared/audiofolders/list1/01track.mp3
+        #                                       /what-a-great-track.mp3
+        #
+        # $PATHDATA/../shared/audiofolders/list987/always-will.mp3
+        #                                         /be.mp3
+        #                                         /playing.mp3
+        #                                         /x-alphabetically.mp3
+        #
+        # $PATHDATA/../shared/audiofolders/webradio/filewithURL.txt
+    
+        # Add info into the log, making it easer to monitor cards
+        echo "Card ID '$CARDID' was used at '$NOW'." > $PATHDATA/../shared/latestID.txt
+    
+    	# Look for human readable shortcut in folder 'shortcuts'
+    	# check if CARDID has a text file by the same name - which would contain the human readable folder name
+    	if [ -f $PATHDATA/../shared/shortcuts/$CARDID ]
+    	then
+            # Read human readable shortcut from file
+            FOLDERNAME=`cat $PATHDATA/../shared/shortcuts/$CARDID`
+            # Add info into the log, making it easer to monitor cards
+            echo "This ID has been used before." >> $PATHDATA/../shared/latestID.txt
+    	else
+            # Human readable shortcut does not exists, so create one with the content $CARDID
+            # this file can later be edited manually over the samba network
+            echo "$CARDID" > $PATHDATA/../shared/shortcuts/$CARDID
+            FOLDERNAME=$CARDID
+            # Add info into the log, making it easer to monitor cards
+            echo "This ID was used for the first time." >> $PATHDATA/../shared/latestID.txt
+        fi
+        # Add info into the log, making it easer to monitor cards
+        echo "The shortcut points to audiofolder '$FOLDERNAME'." >> $PATHDATA/../shared/latestID.txt
+    
+    fi
+fi
+
+
+##############################################################
+# We should now have a folder name with the audio files.
+# Either from prompt of from the card ID processing above
+# Sloppy error check, because we assume the best.
+if [ "$FOLDERNAME" ]; then
+    
+    # if a folder $FOLDERNAME exists, play content
+    if [ -d "$PATHDATA/../shared/audiofolders/$FOLDERNAME" ]
+    then
+        # Check if we have something special to do
+
+        # Podcast?
+        # Read content file names of folder into string
+        SPECIALFORMAT=$(ls $PATHDATA/../shared/audiofolders/$FOLDERNAME | grep podcast.txt)
+        if [ "$SPECIALFORMAT" == "podcast.txt" ]; then
+            # Podcast!
+            # ... to be done ...
+            echo $SPECIALFORMAT
+        else
+            # Nothing special to do, folder with audio files
+            # write playlist to file using the same name as the folder with ending .m3u
+            # wrap $PLAYLIST string in "" to keep line breaks
+            find "$PATHDATA/../shared/audiofolders/$FOLDERNAME" -type f | sort -n > "/tmp/$FOLDERNAME.m3u"
+            VLCPLAYS="/tmp/$FOLDERNAME.m3u"
+        fi
+        # first kill any possible running vlc process => stop playing audio
+        sudo pkill vlc
+    
+        # now start the command line version of vlc loading the playlist
+        # start as a background process (command &) - otherwise the input only works once the playlist finished
+        cvlc --no-video --network-caching=10000 -I rc --rc-host localhost:4212 "$VLCPLAYS" &
+        
+        # NOTE TO SELF: can we get rid off writing a playlist if we play the folder as we do in the index.php?
+        # Currently problem with whitespaces in folder name.
+        # Also, keep in mind the use of stream URLs or YouTube in text files
+        # cvlc --no-video -I rc --rc-host localhost:4212 "$PATHDATA/../shared/audiofolders/$FOLDERNAME" > /dev/null 2>/dev/null &
+    fi
+fi
